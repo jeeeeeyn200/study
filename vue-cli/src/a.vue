0301ee3796c183b3475cdfdca44ccb0903bfbff0
jeeeeeyn200 <jeeeeeyn200@gmail.com>
2024-01-28 17:46:41 +0900

    a.vue - 싱글파일 컴포넌트

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,29 @@
+
+<!--
+싱글파일 컴포넌트
+ 
+var appHeader = {
+    template: '<div></div>',
+    methods: {
+        addNum: function(){}
+    },
+}
+-->
+<!-- vbase 단축키 이용 -->
+<template>
+    <!-- html -->
+    <div>Header</div>
+</template>
+
+<script>
+    //js - 인스턴스 옵션
+    export default {
+        addNum: function(){
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    /* CSS */
+</style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -6,3 +6,11 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+/*
+new Vue({
+  el: '#app',
+  //템플릿 실행시 렌더됨
+  render: h => h(App),
+})
+*/
