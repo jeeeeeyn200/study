0b01287348cb6950b4a7b49703d9c1c5fc9289fb
jeeeeeyn200 <jeeeeeyn200@gmail.com>
2024-01-29 22:48:18 +0900

    app

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,37 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <div>-----------------------------------</div>
-    <hello-world></hello-world>
-    <div>-----------------------------------</div>
-    <HelloWorld></HelloWorld>
-    <div>-----------------------------------</div>
-    <HelloWorld />
-  </div>
+    <div>
+        {{ str }}
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  //인스턴스 옵션 속성 or 캄포넌트 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld
-    //'hello-wolrd' : HelloWorld
-  }
-}
+    export default {
+        data: function() {
+            return {
+                str: 'hi'
+            }
+        },
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
